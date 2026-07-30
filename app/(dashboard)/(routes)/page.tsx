2ea724b278ef82fab38d0a26b7eed1d98e894edd
ahnajafi4252 <ahnajafi4252@gmail.com>
2026-07-30 19:30:04 +0330

    First commit

---
+++ app/(dashboard)/(routes)/page.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@/components/ui/button";
+
+
+export default function Home() {
+  return (
+    <>
+      <div>
+        next app | اپ نکس
+      </div>
+      <Button variant="custom">
+        بر روی من بکلیک
+      </Button>
+    </>
+  );
+}
