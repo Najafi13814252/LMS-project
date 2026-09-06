a0b6d55000ed3130dc29d552e9fa139c1559faa9
ahnajafi4252 <ahnajafi4252@gmail.com>
2026-09-06 16:54:17 +0330

    fixed bug of prisma client

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,5 +1,5 @@
 import "dotenv/config";
-import { PrismaClient } from "./generated/prisma/client";
+import { PrismaClient } from "@/lib/generated/prisma/client";
 import { PrismaMariaDb } from "@prisma/adapter-mariadb"
 
 const adapter = new PrismaMariaDb({
